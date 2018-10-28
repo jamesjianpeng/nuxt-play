cb27e092e500bbe75066a15344d3f765fa8713a9
jamesjianpeng <jamesjianpeng@gmail.com>
2018-10-28 19:47:10 +0800

    fix: delete extra plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,5 @@ export default {
     css:[
         '~/style/index.scss',
         './node_modules/normalize.css/normalize.css'
-    ], // 公共样式
-    plugins: [
-        // { src: '~/plugins/polyfill', ssr: false },
-        '~/plugins/normalize.js',
-    ] // 插件注册
+    ] // 公共样式
 }
